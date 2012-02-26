c72bc6c1b9b6236db0a96dec7dd7a8136004375e
David White <david@spry-soft.com>
2012-02-26 11:11:26 +0000

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DailyGrind::Application.config.secret_token = '9d343f8512b064c317d03459e7a0320d63ffd530dc234d8763631679d630d557c46246a86d58380eb5a122ec9dc28ea0f1cfa2fd827a66bff2a479eb911863fd'
