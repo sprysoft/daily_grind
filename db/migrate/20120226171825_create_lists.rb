6a2bce99b9eaf044d0be647cde7660259903a7c8
David White <david@spry-soft.com>
2012-02-26 19:04:59 +0000

    Added database constraints and indexes.

--- db/migrate/20120226171825_create_lists.rb
+++ db/migrate/20120226171825_create_lists.rb
@@ -1,11 +1,13 @@
 class CreateLists < ActiveRecord::Migration
   def change
     create_table :lists do |t|
-      t.integer :user_id
-      t.string :name
+      t.integer :user_id,     :null => false
+      t.string :name,         :null => false
       t.string :description
 
       t.timestamps
     end
+
+    add_index :lists, :user_id
   end
 end
--- db/migrate/20120226185142_create_tasks.rb
+++ db/migrate/20120226185142_create_tasks.rb
@@ -2,13 +2,16 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.integer :list_id
-      t.integer :user_id
-      t.string :name
+      t.integer :user_id, :null => false
+      t.string :name,     :null => false
       t.text :description
       t.datetime :due_at
       t.datetime :completed_at
 
       t.timestamps
     end
+
+    add_index :tasks, :list_id
+    add_index :tasks, :user_id
   end
 end
